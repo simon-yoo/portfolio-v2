d4a0eb5cf558ad061c2d53771523fab5869def7c
simon-yoo <yoosghn0@gmail.com>
2023-12-01 14:59:52 +1300

    background

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-stone-200 text-stone-800 relative`}
+      >
+        <div className='bg-[#e0afa0] absolute top-[-6rem] -z-10 right-[2rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+        <div className='bg-[#89986d] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[20rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        {children}
+      </body>
     </html>
   )
 }
