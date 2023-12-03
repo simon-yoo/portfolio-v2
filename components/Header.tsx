9b2a135a1c1ea48ee83683df4614933bc0ea1190
simon-yoo <yoosghn0@gmail.com>
2023-12-04 12:34:51 +1300

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <header className='z-[999] relative'>
+      <div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-r-none border border-white border-opacity-40 bg-white bg-opacity-80 shodow-lg shadow-black\[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'></div>
+    </header>
+  )
 }
