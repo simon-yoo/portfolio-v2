b76fc6d2091b274e522dcd7e9141b8e16f3ccab2
simon-yoo <yoosghn0@gmail.com>
2023-12-04 12:54:51 +1300

    images

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,10 @@ import React from 'react'
 import { CgWorkAlt } from 'react-icons/cg'
 import { FaReact } from 'react-icons/fa'
 import { LuGraduationCap } from 'react-icons/lu'
-import corpcommentImg from '@/public/corpcomment.png'
-import rmtdevImg from '@/public/rmtdev.png'
-import wordanalyticsImg from '@/public/wordanalytics.png'
+import wawo from '@/public/images/wa-wo.png'
+import aicompanion from '@/public/images/ai-companion.png'
+import messenger from '@/public/images/messenger.png'
+import worldgym from '@/public/images/world-gym.png'
 
 export const links = [
   {
@@ -66,21 +67,28 @@ export const projectsData = [
     description:
       'I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.',
     tags: ['React', 'Next.js', 'MongoDB', 'Tailwind', 'Prisma'],
-    imageUrl: corpcommentImg,
+    imageUrl: wawo,
   },
   {
     title: 'rmtDev',
     description:
       'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
     tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    imageUrl: rmtdevImg,
+    imageUrl: aicompanion,
   },
   {
     title: 'Word Analytics',
     description:
       'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
     tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-    imageUrl: wordanalyticsImg,
+    imageUrl: messenger,
+  },
+  {
+    title: 'Word Analytics',
+    description:
+      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+    imageUrl: worldgym,
   },
 ] as const
 
