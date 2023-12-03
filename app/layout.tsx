b7e47d024f5f0fa477ddd663cb8866cca2dc55a4
simon-yoo <yoosghn0@gmail.com>
2023-12-04 12:34:48 +1300

    import header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -16,10 +17,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${inter.className} bg-stone-200 text-stone-800 relative`}
+        className={`${inter.className} bg-stone-200 text-stone-800 relative h-[5000px]`}
       >
         <div className='bg-[#e0afa0] absolute top-[-6rem] -z-10 right-[2rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#89986d] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[20rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Header />
         {children}
       </body>
     </html>
