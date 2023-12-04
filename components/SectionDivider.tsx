df8677dd0c33f4b9091e360e8c8f91778fd69ba4
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 09:52:46 +1300

    css

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -1,7 +1,15 @@
+'use client'
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export default function SectionDivider() {
   return (
-    <div className='bg-stone-200 my-24 h-16 w-1 rounded-full hidden sm:block'></div>
+    <motion.div
+      className='bg-stone-400 my-16 h-16 w-1 rounded-2xl hidden sm:block'
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.125 }}
+    ></motion.div>
   )
 }
