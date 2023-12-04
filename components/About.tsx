4ef04d77f6ec5c290c4c81a280cf111d677da2ee
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 10:09:54 +1300

    about

--- components/About.tsx
+++ components/About.tsx
@@ -4,6 +4,21 @@ export default function About() {
   return (
     <section>
       <h2>About me</h2>
+      <p>
+        My journey as a software developer began at Dev Academy, a renowned
+        coding Bootcamp that sparked my passion for problem-solving and
+        innovative thinking. With a solid foundation in coding, I am driven to
+        continuously expand my knowledge and tackle new challenges in the
+        dynamic world of software development.
+      </p>
+      <p>
+        I am eager to contribute my skills, learn from experienced
+        professionals, and make a meaningful impact in the industry. I am
+        excited to connect with like-minded individuals and organisations to
+        collaborate, grow, and thrive in the world of software development.
+        Let's connect and explore opportunities to create innovative solutions
+        together.
+      </p>
     </section>
   )
 }
