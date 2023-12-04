8bc25482e26ca496f58f36269025e38aae84b965
Simon Yoo <yoosghn0@gmail.com>
2023-12-04 19:20:08 +1300

    framer motion for image and emoji

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,21 +1,42 @@
+'use client'
+
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export default function Intro() {
   return (
     <section>
       <div className='flex items-center justify-center'>
-        <div>
-          <Image
-            src='/images/simon.jpeg'
-            width='192'
-            height='192'
-            alt='profile'
-            quality='95'
-            priority={true}
-            className='h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl'
-          />
-          <span className='text-3xl'>👋</span>
+        <div className='relative'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: 'tween', duration: 0.2 }}
+          >
+            <Image
+              src='/images/simon.jpeg'
+              width='192'
+              height='192'
+              alt='profile'
+              quality='95'
+              priority={true}
+              className='h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl'
+            />
+          </motion.div>
+          <motion.span
+            className='text-3xl absolute bottom-0 right-0'
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
         </div>
       </div>
     </section>
