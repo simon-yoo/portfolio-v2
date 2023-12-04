bc2b1497eec97265a461ee82da3df2de0c6b84b3
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 11:34:55 +1300

    projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import SectionHeading from './SectionHeading'
+import { projectsData } from '@/lib/data'
 
 export default function Projects() {
   return (
     <section>
       <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+type ProjectProps = (typeof projectsData)[number]
+
+function Project({ title, description, imageUrl, tags }: ProjectProps) {
+  return (
+    <section>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
     </section>
   )
 }
