2fa2c392ad9d9b0a8fbc4812ca81b1f2e53e4bcb
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 11:20:09 +1300

    about

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import React from 'react'
 import SectionHeading from './SectionHeading'
+import { motion } from 'framer-motion'
 
 export default function About() {
   return (
-    <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+    <motion.section
+      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+    >
       <SectionHeading>About me</SectionHeading>
       <p className='mb-3'>
         My journey as a software developer began at{' '}
@@ -28,6 +36,6 @@ export default function About() {
         Let's connect and explore opportunities to create innovative solutions
         together.
       </p>
-    </section>
+    </motion.section>
   )
 }
