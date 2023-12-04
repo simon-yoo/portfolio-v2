45d1722bc4656ef0989daac9163a6f5e6b9bce79
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 10:17:29 +1300

    about

--- components/About.tsx
+++ components/About.tsx
@@ -2,20 +2,28 @@ import React from 'react'
 
 export default function About() {
   return (
-    <section>
-      <h2>About me</h2>
-      <p>
-        My journey as a software developer began at Dev Academy, a renowned
-        coding Bootcamp that sparked my passion for problem-solving and
-        innovative thinking. With a solid foundation in coding, I am driven to
-        continuously expand my knowledge and tackle new challenges in the
-        dynamic world of software development.
+    <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+      <h2 className='text-3xl font-medium capitalize mb-5'>About me</h2>
+      <p className='mb-3'>
+        My journey as a software developer began at{' '}
+        <span className='font-bold'>Dev Academy</span>, a renowned coding
+        Bootcamp that sparked my passion for{' '}
+        <span className='italic'>problem-solving</span> and{' '}
+        <span className='italic'>innovative thinking</span>. With a solid
+        foundation in coding, I am driven to continuously expand my knowledge
+        and tackle new challenges in the{' '}
+        <span className='font-bold'>dynamic world of software development</span>
+        .
       </p>
       <p>
         I am eager to contribute my skills, learn from experienced
         professionals, and make a meaningful impact in the industry. I am
-        excited to connect with like-minded individuals and organisations to
-        collaborate, grow, and thrive in the world of software development.
+        excited to connect with{' '}
+        <span className='italic'>like-minded individuals</span> and{' '}
+        <span className='italic'>
+          organisations to collaborate, grow, and thrive in the world of
+          software development.
+        </span>
         Let's connect and explore opportunities to create innovative solutions
         together.
       </p>
