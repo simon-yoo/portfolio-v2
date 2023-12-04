16964d03c9d770dd7fd60251fab9287eb64dca28
simon-yoo <yoosghn0@gmail.com>
2023-12-04 13:02:37 +1300

    motion link

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,11 +15,21 @@ export default function Header() {
       ></motion.div>
 
       <nav className='fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
-        <ul>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-stone-400 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
           {links.map((link, i) => (
-            <li key={link.hash}>
-              <Link href={link.hash}>{link.name}</Link>
-            </li>
+            <motion.li
+              className='h-3/4 flex items-center justify-center'
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className='flex w-full items-center justify-center px-3 py-3 hover:text-white hover:border rounded-full hover:bg-stone-700 hover:border-white transition'
+                href={link.hash}
+              >
+                {link.name}
+              </Link>
+            </motion.li>
           ))}
         </ul>
       </nav>
