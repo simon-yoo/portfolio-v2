9246e0134a2d16cb17a5782ea87e63f88220e1ce
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 13:11:27 +1300

    scroll id

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -10,7 +10,7 @@ import { FaGithubSquare } from 'react-icons/fa'
 
 export default function Intro() {
   return (
-    <section className='mb-22 max-w-[50rem]'>
+    <section id='home' className='mb-22 max-w-[50rem] scroll-mt-[100rem]'>
       <div className='flex items-center justify-center'>
         <div className='relative'>
           <motion.div
