529e71fd6afd51246a5300acb1677ba2ed311ef5
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 14:11:55 +1300

    threshold view

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,17 +8,19 @@ import { useInView } from 'react-intersection-observer'
 import { useActiveSectionContext } from '@/context/active-section-context'
 
 export default function Projects() {
-  const { ref, inView } = useInView()
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  })
   const { setActiveSection } = useActiveSectionContext()
 
   useEffect(() => {
     if (inView) {
-      setActiveSection('About')
+      setActiveSection('Projects')
     }
   }, [inView, setActiveSection])
 
   return (
-    <section id='projects' className=' scroll-mt-28'>
+    <section ref={ref} id='projects' className=' scroll-mt-28'>
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
