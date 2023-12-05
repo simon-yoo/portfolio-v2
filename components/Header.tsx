396b8adfc9dfb3c2427cb3ae96db322da072a688
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 13:55:38 +1300

    import activesectioncontext

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import React from 'react'
+import React, { useContext } from 'react'
 import { motion } from 'framer-motion'
 import { links } from '@/lib/data'
 import Link from 'next/link'
 import clsx from 'clsx'
+import { ActiveSectionContext } from '@/context/active-section-context'
 
 export default function Header() {
+  useContext(ActiveSectionContext)
+
   return (
     <header className='z-[999] relative'>
       <motion.div
