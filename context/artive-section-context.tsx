8dc235f4cdbbd1be21a0bf1ebe29cc3cf011a740
Simon Yoo <yoosghn0@gmail.com>
2023-12-05 13:39:38 +1300

    create activesectioncontext

---
+++ context/artive-section-context.tsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react'
+
+export default function ArtiveSectionContext() {
+  const [activeSection, setActiveSection] = useState('Home')
+  return <div>ArtiveSectionContext</div>
+}
