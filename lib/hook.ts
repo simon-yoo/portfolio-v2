bdaea15b92c5ecae164ead31e3ae68d70af0cb6f
simon-yoo <yoosghn0@gmail.com>
2023-12-07 09:36:31 +1300

    create custom hook

---
+++ lib/hook.ts
@@ -0,0 +1,18 @@
+import { useActiveSectionContext } from '@/context/active-section-context'
+import { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+
+export function useSectionInView() {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  })
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection('Home')
+    }
+  }, [inView, setActiveSection, timeOfLastClick])
+
+  return { ref, inView }
+}
