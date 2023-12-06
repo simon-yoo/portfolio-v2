7ef1688a9640993878652c795f7a02092353dab2
simon-yoo <yoosghn0@gmail.com>
2023-12-07 09:44:59 +1300

    useSectionInView hook

--- components/About.tsx
+++ components/About.tsx
@@ -5,18 +5,10 @@ import SectionHeading from './SectionHeading'
 import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import { useActiveSectionContext } from '@/context/active-section-context'
+import { useSectionInView } from '@/lib/hook'
 
 export default function About() {
-  const { ref, inView } = useInView({
-    threshold: 0.75,
-  })
-  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
-
-  useEffect(() => {
-    if (inView && Date.now() - timeOfLastClick > 1000) {
-      setActiveSection('About')
-    }
-  }, [inView, setActiveSection, timeOfLastClick])
+  const { ref } = useSectionInView('About')
 
   return (
     <motion.section
