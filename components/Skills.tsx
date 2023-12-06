4d9c85430087560069a35d2aa3d991785be0ac06
simon-yoo <yoosghn0@gmail.com>
2023-12-07 10:12:33 +1300

    skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,6 +4,21 @@ import React from 'react'
 import SectionHeading from './SectionHeading'
 import { useSectionInView } from '@/lib/hook'
 import { skillsData } from '@/lib/data'
+import { motion } from 'framer-motion'
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+}
 
 export default function Skills() {
   const { ref } = useSectionInView('Skills')
@@ -15,12 +30,19 @@ export default function Skills() {
       <SectionHeading>My skills</SectionHeading>
       <ul className='flex flex-wrap justify-center text-lg gap-4 mt-10 text-stone-600'>
         {skillsData.map((skill, index) => (
-          <li
+          <motion.li
+            variants={fadeInAnimationVariants}
+            initial='initial'
+            whileInView='animate'
+            viewport={{
+              once: true,
+            }}
+            custom={index}
             className='bg-stone-200 border border-black/[0.1] rounded-md px-5 py-3 shadow-2xl'
             key={index}
           >
             {skill}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </section>
