ae17cbb589e79d40ad0e49473aa84df954bcc4ea
simon-yoo <yoosghn0@gmail.com>
2023-12-07 10:06:13 +1300

    skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,17 +1,26 @@
+'use client'
+
 import React from 'react'
 import SectionHeading from './SectionHeading'
 import { useSectionInView } from '@/lib/hook'
-import { link } from 'fs'
 import { skillsData } from '@/lib/data'
 
 export default function Skills() {
   const { ref } = useSectionInView('Skills')
   return (
-    <section>
+    <section
+      ref={ref}
+      className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'
+    >
       <SectionHeading>My skills</SectionHeading>
-      <ul>
+      <ul className='flex flex-wrap justify-center text-lg gap-2 text-stone-600'>
         {skillsData.map((skill, index) => (
-          <li key={index}>{skill}</li>
+          <li
+            className='bg-stone-200 border border-black/[0.1] rounded-md px-5 py-3 shadow-2xl'
+            key={index}
+          >
+            {skill}
+          </li>
         ))}
       </ul>
     </section>
