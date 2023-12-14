61736b788b0a756bbf06e459eeddf20529e1883a
simon-yoo <yoosghn0@gmail.com>
2023-12-15 11:38:03 +1300

    css

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -14,7 +14,7 @@ import { experiencesData } from '@/lib/data'
 export default function Experience() {
   const { ref } = useSectionInView('Experience')
   return (
-    <section id='experience' ref={ref}>
+    <section id='experience' ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor='#d6d3d1'>
         {experiencesData.map((item, index) => (
