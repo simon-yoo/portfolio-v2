fe761cc5ac1ccbe9d363f34199415d6d74b590c3
simon-yoo <yoosghn0@gmail.com>
2023-12-15 10:29:30 +1300

    experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,5 +1,21 @@
+'use client'
+
+import { useSectionInView } from '@/lib/hook'
 import React from 'react'
+import SectionHeading from './SectionHeading'
+import { motion } from 'framer-motion'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
 
 export default function Experience() {
-  return <div>Experience</div>
+  const { ref } = useSectionInView('Experience')
+  return (
+    <section id='experience' ref={ref}>
+      <SectionHeading>My experience</SectionHeading>
+      <VerticalTimeline></VerticalTimeline>
+    </section>
+  )
 }
