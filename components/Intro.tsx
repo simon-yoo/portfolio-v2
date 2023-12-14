9fc5162071fef78e1bc6a4e0f06d733ee30da1c0
simon-yoo <yoosghn0@gmail.com>
2023-12-15 12:01:36 +1300

    setActiveSection

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,9 +8,11 @@ import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
 import { HiDownload } from 'react-icons/hi'
 import { FaGithubSquare } from 'react-icons/fa'
 import { useSectionInView } from '@/lib/hook'
+import { useActiveSectionContext } from '@/context/active-section-context'
 ;``
 export default function Intro() {
   const { ref } = useSectionInView('Home', 0.5)
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext()
 
   return (
     <section
@@ -70,6 +72,10 @@ export default function Intro() {
         <Link
           href='#contact'
           className='group shadow-2xl  bg-stone-700 text-stone-100 mx-1 px-7 py-3 flex items-center gap-2 rounded-lg outline-none focus:scale-110 hover:scale-110 hover:rounded-2xl hover:bg-stone-200 hover:text-stone-600 hover:border-stone/10 active:scale-105 transition'
+          onClick={() => {
+            setActiveSection('Contact')
+            setTimeOfLastClick(Date.now())
+          }}
         >
           Contact me{' '}
           <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
