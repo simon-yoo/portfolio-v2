c6b3a08fbc41e6491d93b07e106a271b300690c6
simon-yoo <yoosghn0@gmail.com>
2023-12-15 11:51:07 +1300

    css

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,9 +4,9 @@ import { FaPaperPlane } from 'react-icons/fa'
 
 export default function Contact() {
   return (
-    <section id='contact'>
+    <section id='contact' className='mb-20 sm:mb-28 w-[min(100% ,38rem)]'>
       <SectionHeading>Contact me</SectionHeading>
-      <p>
+      <p className='text-stone-600'>
         Please contact me directly at{' '}
         <a href='mailto:yoosghn0@gmail.com' className='underline'>
           yoosghn0@gmail.com
@@ -14,11 +14,24 @@ export default function Contact() {
         or through this form.
       </p>
 
-      <form action=''>
-        <input type='email' />
-        <textarea name='' id=''></textarea>
-        <button type='submit'>
-          Submit <FaPaperPlane />
+      <form action='' className='mt-10 flex flex-col '>
+        <input
+          type='email'
+          className='h-14 px-4 rounded-lg borderBlack'
+          placeholder='Your email'
+        />
+        <textarea
+          name=''
+          id=''
+          className='h-52 my-3 rounded-lg borderBlack p-4'
+          placeholder='Your message'
+        />
+        <button
+          type='submit'
+          className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-stone-700 text-stone-100 rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-stone-900 hover:text-stone-100'
+        >
+          Submit{' '}
+          <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 ' />
         </button>
       </form>
     </section>
