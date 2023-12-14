c56ceae2bc5e3f1be281171adab1c7af8518971e
simon-yoo <yoosghn0@gmail.com>
2023-12-15 11:27:44 +1300

    fix error

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -16,13 +16,32 @@ export default function Experience() {
   return (
     <section id='experience' ref={ref}>
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline>
+      <VerticalTimeline lineColor='#d6d3d1'>
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
-            <VerticalTimelineElement>
-              <h3>{item.title}</h3>
-              <p>{item.location}</p>
-              <p>{item.description}</p>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: '#d6d3d1',
+                boxShadow: 'none',
+                border: '1px solid rgba(0,0,0,0.05)',
+                textAlign: 'left',
+                padding: '1.3rem  2rem',
+              }}
+              contentArrowStyle={{
+                borderRight: '0.4rem solid #9ca3af',
+              }}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background: '#9ca3af',
+                fontSize: '1.5rem',
+              }}
+            >
+              <h3 className='font-semibold capitalize'>{item.title}</h3>
+              <p className='font-normal !mt-0'>{item.location}</p>
+              <p className='!mt-1 !font-normal text-stone-500'>
+                {item.description}
+              </p>
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
