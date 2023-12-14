7844dba3e5c0f1417b2fb694bf4f1d90d636c644
simon-yoo <yoosghn0@gmail.com>
2023-12-15 11:41:03 +1300

    create form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import SectionHeading from './SectionHeading'
+import { FaPaperPlane } from 'react-icons/fa'
 
 export default function Contact() {
-  return <div>Contact</div>
+  return (
+    <section id='contact'>
+      <SectionHeading>Contact me</SectionHeading>
+      <p>
+        Please contact me directly at{' '}
+        <a href='mailto:yoosghn0@gmail.com' className='underline'>
+          yoosghn0@gmail.com
+        </a>
+        or through this form.
+      </p>
+
+      <form action=''>
+        <input type='email' />
+        <textarea name='' id=''></textarea>
+        <button type='submit'>
+          Submit <FaPaperPlane />
+        </button>
+      </form>
+    </section>
+  )
 }
