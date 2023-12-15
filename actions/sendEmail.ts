5ab1f8a8eee4864c8b4f6fb62438cae0fa4ef666
simon-yoo <yoosghn0@gmail.com>
2023-12-15 14:46:17 +1300

    validateString

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,6 +1,37 @@
 'use server'
+
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+const validateString = (value: unknown, maxLength: number) => {
+  if (!value || typeof value !== 'string' || value.length > maxLength) {
+    return false
+  }
+  return true
+}
+
 export const sendEmail = async (formData: FormData) => {
-  console.log('Runnig on server')
-  console.log(formData.get('senderEmail'))
-  console.log(formData.get('message'))
+  const senderEmail = formData.get('email')
+  const message = formData.get('message')
+
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: 'Invalid sender email',
+    }
+  }
+
+  if (!validateString(message, 5000)) {
+    return {
+      error: 'Invalid message',
+    }
+  }
+
+  resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: 'yoosghn0@gmail.com',
+    subject: 'Message from portfolio',
+    reply_to: senderEmail,
+    text: message,
+  })
 }
