b2297337a80910bc59fea40cadb343983fd11a5b
simon-yoo <yoosghn0@gmail.com>
2024-01-02 10:15:51 +1300

    geterrormessage

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -2,11 +2,11 @@
 
 import { validateString } from '@/lib/utils'
 import { Resend } from 'resend'
-
+import { getErrorMessage } from '@/lib/utils'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendEmail = async (formData: FormData) => {
-  const senderEmail = formData.get('email')
+  const senderEmail = formData.get('senderEmail')
   const message = formData.get('message')
 
   if (!validateString(senderEmail, 500)) {
@@ -23,13 +23,15 @@ export const sendEmail = async (formData: FormData) => {
 
   try {
     await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: 'Contact Form <onboarding@resend.dev>',
       to: 'yoosghn0@gmail.com',
       subject: 'Message from portfolio',
       reply_to: senderEmail as string,
       text: message as string,
     })
-  } catch (error) {
-    console.log(error)
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    }
   }
 }
