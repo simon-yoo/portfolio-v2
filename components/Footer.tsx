dabe1c655d638f51436f5722a99be2379d2089cb
simon-yoo <yoosghn0@gmail.com>
2024-01-16 15:28:59 +1300

    css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>
-      <small>&copy; 2024 All rights reserved. </small>
-      <p>
-        <span>About this website:</span> built with React & Next.js (App Router
-        & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email
-        & Resend, Vercel hosting.
+    <footer className='mb-10 px-4 text-center text-stone-700'>
+      <small className='mb-2 block text-xs'>
+        &copy; 2024 All rights reserved.{' '}
+      </small>
+      <p className='text-xs'>
+        <span className='font-semibold'>About this website:</span> built with
+        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Email & Resend, Vercel hosting.
       </p>
     </footer>
   )
