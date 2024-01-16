82557b4c9fb115f6ec1b7d0957ce70a7febcece9
simon-yoo <yoosghn0@gmail.com>
2024-01-17 12:09:07 +1300

    darkmode

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,42 +1,15 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import { useTheme } from '@/context/ThemeContext'
+import React from 'react'
 import { BsSun, BsMoon } from 'react-icons/bs'
 
-type Theme = 'light' | 'dark'
-
 export default function ThemeSwitch() {
-  const [theme, setTheme] = useState<Theme>('light')
-
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('dark')
-      window.localStorage.setItem('theme', 'dark')
-      document.documentElement.classList.add('dark')
-    } else {
-      setTheme('light')
-      window.localStorage.setItem('theme', 'light')
-      document.documentElement.classList.remove('dark')
-    }
-  }
-
-  useEffect(() => {
-    const localTheme = window.localStorage.getItem('theme') as Theme | null
-
-    if (localTheme) {
-      setTheme(localTheme)
-      if (localTheme === 'dark') {
-        document.documentElement.classList.add('dark')
-      }
-    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      setTheme('dark')
-      document.documentElement.classList.add('dark')
-    }
-  }, [])
+  const { theme, toggleTheme } = useTheme()
 
   return (
     <button
-      className='fixed top-6 right-5 bg-stone-200 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-stone-700'
+      className='fixed top-24 right-5 md:top-6 md:right-5 bg-stone-200 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-stone-700'
       onClick={toggleTheme}
     >
       {theme === 'light' ? <BsSun /> : <BsMoon />}
