0e52ab38ac3de6cf538acd6162096d7ecaf1e82a
simon-yoo <yoosghn0@gmail.com>
2024-01-16 15:27:21 +1300

    Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2024 All rights reserved. </small>
+      <p>
+        <span>About this website:</span> built with React & Next.js (App Router
+        & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email
+        & Resend, Vercel hosting.
+      </p>
+    </footer>
+  )
+}
