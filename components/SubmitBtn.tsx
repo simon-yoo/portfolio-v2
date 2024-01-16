a4d9f68d48986c6c3549b96d3d7fa5cefd806e3d
simon-yoo <yoosghn0@gmail.com>
2024-01-16 14:37:59 +1300

    submitbtn

--- components/SubmitBtn.tsx
+++ components/SubmitBtn.tsx
@@ -8,10 +8,17 @@ export default function SubmitBtn() {
   return (
     <button
       type='submit'
-      className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-stone-700 text-stone-100 rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-stone-900 hover:text-stone-100'
+      className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-stone-700 text-stone-100 rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-stone-900 hover:text-stone-100 disabled:scale-100 disabled:bg-opacity-65'
+      disabled={pending}
     >
-      Submit{' '}
-      <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 ' />
+      {pending ? (
+        <div className='h-5 w-5 animate-spin rounded-full border-b-2 border-white'></div>
+      ) : (
+        <>
+          Submit{' '}
+          <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 ' />
+        </>
+      )}
     </button>
   )
 }
