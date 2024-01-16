d985f96c62753ba5582c91d9531186c958954f94
simon-yoo <yoosghn0@gmail.com>
2024-01-17 12:08:56 +1300

    darkmode

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { links } from '@/lib/data'
 import Link from 'next/link'
 import clsx from 'clsx'
 import { useActiveSectionContext } from '@/context/active-section-context'
+import ThemeSwitch from './ThemeSwitch'
 
 export default function Header() {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
@@ -13,13 +14,13 @@ export default function Header() {
   return (
     <header className='z-[999] relative'>
       <motion.div
-        className='fixed top-0 left-1/2 h-[4.5rem] w-full border border-white border-opacity-40 bg-stone-200 bg-opacity-80 shodow-lg shadow-black\[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] rounded-lg dark:bg-stone-700'
+        className='fixed top-0 left-1/2 h-[4.5rem] w-full border border-white border-opacity-40 bg-stone-200 bg-opacity-80 shodow-lg shadow-black\[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] rounded-lg dark:bg-stone-400/70'
         initial={{ y: -100, x: '-50%', opacity: 0 }}
         animate={{ y: 0, x: '-50%', opacity: 1 }}
       ></motion.div>
 
       <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
-        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-stone-500 sm:w-[initial] sm:flex-nowrap sm:gap-5 dark:text-stone-100 dark:text-opacity-90'>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-stone-500 sm:w-[initial] sm:flex-nowrap sm:gap-5 '>
           {links.map((link, i) => (
             <motion.li
               className='h-3/4 flex items-center justify-center relative'
@@ -29,7 +30,7 @@ export default function Header() {
             >
               <Link
                 className={clsx(
-                  'flex w-full items-center justify-center px-3 py-3 hover:text-stone-700 hover:font-bold hover:border-white transition',
+                  'flex w-full items-center justify-center px-3 py-3 hover:text-stone-700 hover:font-bold dark:hover:text-stone-800 hover:border-white transition dark:text-stone-600',
                   { 'text-stone-900 font-bold': activeSection === link.name }
                 )}
                 href={link.hash}
@@ -41,7 +42,7 @@ export default function Header() {
                 {link.name}
                 {link.name === activeSection && (
                   <motion.span
-                    className='bg-stone-400 rounded-full absolute inset-0 -z-10'
+                    className='bg-stone-100 rounded-full absolute inset-0 -z-10 dark:bg-stone-100/90 '
                     layoutId='activeSection'
                     transition={{
                       type: 'spring',
