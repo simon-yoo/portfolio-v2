bbb54dbdc74a19360e6b2e103de6fd9704c2e223
simon-yoo <yoosghn0@gmail.com>
2024-01-17 12:09:00 +1300

    darkmode

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 export default function SectionDivider() {
   return (
     <motion.div
-      className='bg-stone-400 my-16 h-16 w-1 rounded-2xl hidden sm:block'
+      className='bg-stone-400 my-16 h-16 w-1 rounded-2xl hidden sm:block dark:bg-stone-100/70'
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
