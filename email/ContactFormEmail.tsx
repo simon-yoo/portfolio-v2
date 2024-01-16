72fb853646ab7816ca119c57c88f2653c593f9cb
simon-yoo <yoosghn0@gmail.com>
2024-01-16 14:19:48 +1300

    css

--- email/ContactFormEmail.tsx
+++ email/ContactFormEmail.tsx
@@ -28,10 +28,10 @@ export default function ContactFormEmail({
       <Head />
       <Preview>New message from your portfolio site</Preview>
       <Tailwind>
-        <Body>
+        <Body className='bg-gray-100 text-black'>
           <Container>
-            <Section>
-              <Heading>
+            <Section className='bg-white borderBlack my-10 px-10 py-4 rounded-md'>
+              <Heading className='leading-tight'>
                 You received the following message from the contact form
               </Heading>
               <Text>{message}</Text>
