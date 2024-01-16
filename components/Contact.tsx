9ccd5da79c70f5eba79de9053892e305901562de
simon-yoo <yoosghn0@gmail.com>
2024-01-16 14:35:16 +1300

    submitbtn

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import SectionHeading from './SectionHeading'
-import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 import { useSectionInView } from '@/lib/hook'
 import { sendEmail } from '@/actions/sendEmail'
+import SubmitBtn from './SubmitBtn'
+
 export default function Contact() {
   const { ref } = useSectionInView('Contact')
 
@@ -52,13 +53,7 @@ export default function Contact() {
           className='h-52 my-3 rounded-lg borderBlack p-4'
           placeholder='Your message'
         />
-        <button
-          type='submit'
-          className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-stone-700 text-stone-100 rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-stone-900 hover:text-stone-100'
-        >
-          Submit{' '}
-          <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 ' />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   )
