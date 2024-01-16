dda0f18264ad3ca31bb9407bd68b9758dbda7cb2
simon-yoo <yoosghn0@gmail.com>
2024-01-17 12:08:53 +1300

    darkmode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='mb-10 px-4 text-center text-stone-700'>
+    <footer className='mb-10 px-4 text-center text-stone-700 dark:text-stone-100/70'>
       <small className='mb-2 block text-xs'>
         &copy; 2024 All rights reserved.{' '}
       </small>
