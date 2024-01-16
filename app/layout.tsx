936871828767e019b7268cbe06d934b392ad6044
simon-yoo <yoosghn0@gmail.com>
2024-01-17 08:49:20 +1300

    dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import { Toaster } from 'react-hot-toast'
 import Footer from '@/components/Footer'
+import ThemeSwitch from '@/components/ThemeSwitch'
 
 export const metadata = {
   title: 'Simon | Personal Protfolio',
@@ -16,8 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className='!scroll-smooth'>
-      <body className='bg-stone-200 text-stone-800 relative pt-28 sm:pt-36'>
-        <div className='bg-[#e0afa0] absolute top-[-6rem] -z-10 right-[2rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+      <body className='bg-stone-200 text-stone-800 relative pt-28 sm:pt-36 dark:bg-stone-800 dark:text-stone-100 dark:text-opacity-90'>
+        <div className='bg-[#e0afa0] absolute top-[-6rem] -z-10 right-[2rem] h-[56rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#89986d] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[20rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
 
         <ActiveSectionContextProvider>
@@ -26,6 +27,7 @@ export default function RootLayout({
           <Footer />
           <Toaster position='bottom-center' />
         </ActiveSectionContextProvider>
+        <ThemeSwitch />
       </body>
     </html>
   )
