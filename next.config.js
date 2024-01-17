1b1bcfd95c6f1f8895bb893c5a55867f81950199
simon-yoo <yoosghn0@gmail.com>
2024-01-17 14:29:54 +1300

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
 const nextConfig = {
   experimental: {
     serverActions: true,
-  },
-  output: 'standalone',
-  webpack: (config, { isServer }) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'components')
-    return config
   },
 }
 
