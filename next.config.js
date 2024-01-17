c07d17dc9fd531a260ea148f79b517e3a90903f7
simon-yoo <yoosghn0@gmail.com>
2024-01-17 14:03:44 +1300

    fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['~'] = path.join(__dirname, 'src')
+    return config
+  },
 }
 
 module.exports = nextConfig
