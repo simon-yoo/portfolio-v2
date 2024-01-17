88f4a809c8d497be8aa523fe0cf64d4d3b95390d
simon-yoo <yoosghn0@gmail.com>
2024-01-17 13:46:22 +1300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from '@/components/About'
 import Contact from '@/components/Contact'
 import Experience from '@/components/Experience'
 import Intro from '@/components/Intro'
-import Projects from '@/components/Projects'
+import Projects from '../components/Projects'
 import SectionDivider from '@/components/SectionDivider'
 import Skills from '@/components/Skills'
 
