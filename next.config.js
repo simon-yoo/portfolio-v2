bdf9dc03992496cb2811fe97d46b9d4ee58ec9df
simon-yoo <yoosghn0@gmail.com>
2024-01-17 14:04:25 +1300

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   },
   output: 'standalone',
   webpack: (config, { isServer }) => {
-    config.resolve.alias['~'] = path.join(__dirname, 'src')
+    config.resolve.alias['@'] = path.join(__dirname, 'src')
     return config
   },
 }
